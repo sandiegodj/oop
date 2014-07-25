49362b37005c0638dca3172a2a3d5bf4258e48eb
sandiegodj <davidjanczyn@gmail.com>
2014-07-24 22:08:37 -0700

    games working well

---
+++ mastermind.rb
@@ -0,0 +1,89 @@
+#maybe put a check to make sure player does not input the same letter twice
+#and an AI if I find the time
+
+  def initialize
+    @@code = []
+    generate_code
+    display_instructions
+    gameplay
+  end
+
+  def generate_code
+      color = %w(A B C D E F).shuffle!
+      4.times do |i|
+        @@code << color[i]
+      end
+      puts @@code
+  end
+  def display_instructions
+    puts "'Mastermind' The Game"
+    sleep(2)
+    puts "The computer will create a secret pattern of 4 unique letters ranging from A to F."
+    sleep(3)
+    puts "You have twelve chances to guess the correct pattern."
+    sleep(3)
+    puts "After each guess, you will recieve feedback:"
+    sleep(2)
+    puts "A 'Z' will represent a correct letter in the correct spot,"
+    sleep(2)
+    puts "and a 'X' will represent a correct color in a wrong spot."
+    sleep(2)
+    puts "Good luck!"
+    sleep(1)
+    puts "\n\n\n"
+  end
+
+  def gameplay
+
+    @turns = 1
+    @win = false
+    until @win
+      if @turns == 12
+      #game_over method
+      end
+      print "Guess:"
+      @guess = gets.chomp.upcase.split('').to_a
+      placement
+      correct_color
+    end
+  end
+
+  def placement
+    if @guess.count > 4
+      puts "You entered too many letters! Try again chump"
+    end
+    @feedback = %w(No No No No)
+    correct = 0
+    @temp = @guess
+    n = 0    
+    @guess.each_with_index do |guess, n|
+      if guess == @@code[n]
+        @feedback[n] = "Z"
+        correct += 1
+        @temp[n] = " "
+      end
+    
+    end
+
+    if correct == 4
+      @win = true
+    end
+
+  end
+
+  def correct_color
+
+    correct = 0
+    @temp.each_with_index do |guess, n| 
+      if @@code.include? guess
+        correct += 1
+        @feedback[n] = "X"
+      end
+    end
+    print @feedback
+    puts ""
+    @turns += 1
+  end
+end
+
+game = Game.new
---
+++ tic_tac_toe.rb
@@ -0,0 +1,126 @@
+class Game
+
+  private
+  def initialize(p1, p2)
+
+    @player_1 = Player.new(p1)
+    @player_2 = Player.new(p2)
+
+    @@winCases = [ [0, 4, 8], [3, 4, 5], [1, 4, 7], [2, 4, 6], 
+                  [0, 1, 2], [0, 3, 6], [6, 7, 8], [2, 5, 8] ]
+    @move_count = 1
+    @grid = (1..9).to_a
+    game_play
+  end
+
+  def game_play
+
+    @win = false
+    clear
+    draw_board
+
+    until @win
+      move
+      winning_combo?
+      if winning_combo? == false and @stalemate == true
+        stalemate
+        @win = true
+      end
+    end
+    play_again?
+  end
+
+  def draw_board
+    @grid.each_with_index do |square, index|
+      if (index+1) % 3 == 0
+        print " #{square} \n"
+        print "-----------\n" if index != 8
+      else
+        print " #{square} |"
+      end
+    end
+  end
+
+  def move
+
+    if @move_count == 9
+      @stalemate = true
+    end
+
+      @move_count % 2 == 0 ? @turn = @player_2 : @turn = @player_1
+      puts "It's your move, #{@turn.name}"
+      input = gets.chomp.to_i
+    if valid_move?(input)  
+      @turn == @player_1 ? @grid[input - 1] = 'X': @grid[input - 1] = 'O'
+
+      @move_count += 1
+      clear
+      draw_board
+    else
+      return
+    end
+  end
+
+
+  def valid_move?(input)
+    if @grid[input - 1] == 'O' or @grid[input - 1] == 'X'
+    clear 
+    draw_board
+    puts "Invalid move"
+    return false  
+    end
+    return true
+  end
+
+
+  #not sure why this in printing the win twice
+  def winning_combo?
+    player = nil
+    @@winCases.each do |a|
+      player = @grid[a[0]]
+      if (@grid[a[0]] == @grid[a[1]]) && (@grid[a[1]] == @grid[a[2]])
+        puts "#{@turn.name} WON!"
+        @win = true
+        @stalemate = false
+        return true
+      end
+    end
+    return false
+  end
+
+
+  class Player
+    attr_accessor :name
+    def initialize(name)
+      @name = name.downcase
+    end
+  end
+
+  def clear
+    system "clear" or system "cls"
+  end
+
+  def stalemate
+    puts "Game ended in a stalemate."
+  end
+
+  def play_again?
+    puts "Play again? y or n?"
+    input = gets.chomp
+    if input == 'y' 
+      system "ruby tic_tac_toe.rb"
+    end
+  end
+end
+
+
+system "clear"
+puts "Welcome to Tic Tac Toe"
+sleep(2)
+system "clear"
+puts "Player 1 name:"
+p1 = gets.chomp
+system "clear"
+puts "Player 2 name:"
+p2 = gets.chomp
+board = Game.new(p1, p2)
